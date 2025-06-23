07a421fc8bbdb521f10aff432bad49013707a6fa
ethicnology <ethicnology@pm.me>
2025-06-22 21:00:48 -0400

    feat: update decoder with proper URL decoding

--- lib/src/converters/dart_bip21_decoder.dart
+++ lib/src/converters/dart_bip21_decoder.dart
@@ -1,40 +1,57 @@
 import 'dart:convert';
 import 'dart:core';
 
-import 'package:dart_bip21/src/models/bip21.dart';
+import 'package:bip21_uri/src/bip21_uri_model.dart';
+import 'package:bip21_uri/utils.dart';
 
 /// The canonical instance of [BIP21Decoder].
 const bip21Decoder = BIP21Decoder();
 
 /// BIP21 Decoder
 ///
-/// A converter that encodes [String] to [BIP21].
-class BIP21Decoder extends Converter<String, BIP21> {
+/// A converter that encodes [String] to [Bip21Uri].
+class BIP21Decoder extends Converter<String, Bip21Uri> {
   const BIP21Decoder();
 
   @override
-  BIP21 convert(String input, [String urnScheme = 'bitcoin']) {
-    String urnSchemeActual = input.substring(0, urnScheme.length).toLowerCase();
-    if (urnSchemeActual != urnScheme || input[urnScheme.length] != ':') {
-      throw Exception('Invalid BIP21 URI: $input');
-    }
-    int split = input.indexOf('?');
-    String? address =
-        input.substring(urnScheme.length + 1, split == -1 ? null : split);
-    String query = split == -1 ? '' : input.substring(split + 1);
-    Map<String, dynamic> options =
-        Map<String, dynamic>.from(Uri.splitQueryString(query));
+  Bip21Uri convert(String input) {
+    final parts = input.split(':');
+    final scheme = parts.first.toLowerCase();
 
-    if (options.containsKey('amount')) {
-      num? amount = num.tryParse(options['amount'].toString());
-      if (amount == null) {
-        throw FormatException('Invalid amount');
+    Map<String, dynamic> options = {};
+    String address = parts.last.split('?').first;
+    if (!input.contains('?')) {
+      address = parts.last;
+    } else {
+      String query = parts.last.split('?').last;
+      options = Map<String, dynamic>.from(Uri.splitQueryString(query));
+    }
+
+    double? amount;
+    if (options['amount'] != null) {
+      try {
+        amount = double.parse(options['amount']!);
+      } catch (_) {
+        throw 'Invalid amount';
       }
-      if (!amount.isFinite) throw FormatException('Invalid amount');
-      if (amount < 0) throw FormatException('Invalid amount');
-      options['amount'] = amount;
     }
 
-    return BIP21(address, options, urnScheme);
+    final label =
+        options['label'] != null ? decodeParam(options['label']!) : null;
+    final message =
+        options['message'] != null ? decodeParam(options['message']!) : null;
+
+    options.removeWhere(
+      (key, value) => ['amount', 'label', 'message'].contains(key),
+    );
+
+    return Bip21Uri(
+      scheme: scheme,
+      address: address,
+      amount: amount,
+      label: label,
+      message: message,
+      options: options,
+    );
   }
 }
