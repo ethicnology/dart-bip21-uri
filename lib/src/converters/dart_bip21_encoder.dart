f9f0bffe4a25e2d05ddaeff572be2074732b6b76
ethicnology <ethicnology@pm.me>
2025-06-22 21:00:45 -0400

    feat: update encoder with proper URL encoding

--- lib/src/converters/dart_bip21_encoder.dart
+++ lib/src/converters/dart_bip21_encoder.dart
@@ -1,37 +1,39 @@
 import 'dart:convert';
 import 'dart:core';
 
-import 'package:dart_bip21/src/models/bip21.dart';
+import 'package:bip21_uri/src/bip21_uri_model.dart';
+import 'package:bip21_uri/utils.dart';
 
 /// The canonical instance of [BIP21Encoder].
 const bip21Encoder = BIP21Encoder();
 
 /// BIP21 Encoder
 ///
-/// A converter that encodes [BIP21] to [String].
-class BIP21Encoder extends Converter<BIP21, String> {
+/// A converter that encodes [Bip21Uri] to [String].
+class BIP21Encoder extends Converter<Bip21Uri, String> {
   const BIP21Encoder();
 
   @override
-  String convert(BIP21 input) {
-    Map<String, dynamic> options = Map<String, dynamic>.from(input.options).map(
-        (key, value) => MapEntry(Uri.encodeQueryComponent(key),
-            value is num ? value : Uri.encodeQueryComponent(value)));
-    if (options['amount'] != null) {
-      num? amount = num.tryParse(options['amount'].toString());
-      if (amount == null || !amount.isFinite || amount < 0) {
-        throw FormatException('Invalid amount');
-      }
-    }
+  String convert(Bip21Uri input) {
+    final options = Map<String, dynamic>.from(input.options).map(
+      (key, value) => MapEntry(
+        encodeParam(key),
+        encodeParam(value.toString()),
+      ),
+    );
 
     String query = options.keys.map((key) => '$key=${options[key]}').join('&');
 
+    final params = [
+      if (input.amount != null) 'amount=${input.amount}',
+      if (input.label != null) 'label=${encodeParam(input.label!)}',
+      if (input.message != null) 'message=${encodeParam(input.message!)}',
+      if (options.keys.isNotEmpty) query
+    ];
+
     return [
-      input.urnScheme,
-      ':',
-      input.address,
-      options.keys.isNotEmpty ? '?' : '',
-      query
+      '${input.scheme}:${input.address}',
+      if (params.isNotEmpty) '?${params.join('&')}',
     ].join('');
   }
 }
